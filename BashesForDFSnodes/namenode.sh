c30f47de6f5b43109fb476a12e90b71119d5b547
jafar <jafer.ba@Gmail.com>
2019-11-18 21:44:44 +0300

    ADDED CONFIGURATION ON AWS

--- BashesForDFSnodes/namenode.sh
+++ BashesForDFSnodes/namenode.sh
@@ -3,4 +3,6 @@ snap install docker
 mkdir /home/ubuntu/namenode
 echo "26200" >>/home/ubuntu/namenode/namenode_hosts.conf
 echo "{\"hussain\":\"123456\",\"almir\":\"123456\",\"jafar\":\"123456\"}" >>/home/ubuntu/namenode/users.conf
+cd /home/ubuntu
+sudo docker run -v $(pwd):/namenode -it jafarbadour/namenode-dfs
 
