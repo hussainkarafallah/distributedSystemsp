a32e16e1cbd453d93d11fe8375695fa8af540f35
jafar <jafer.ba@Gmail.com>
2019-11-18 21:53:27 +0300

    ADDED CONFIGURATION ON AWS

--- BashesForDFSnodes/client.sh
+++ BashesForDFSnodes/client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 snap install docker
 mkdir /home/ubuntu/client
-echo "18.217.199.246 26200 client" >>/home/ubuntu/client/client_hosts.conf
+echo "10.0.1.72  26200 client" >>/home/ubuntu/client/client_hosts.conf
--- BashesForDFSnodes/datanode.sh
+++ BashesForDFSnodes/datanode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 snap install docker
 mkdir /home/ubuntu/datanode
-echo "25011 18.217.199.246 26200 datanode" >>/home/ubuntu/datanode/datanode_hosts.conf
+echo "25011 10.0.1.72  26200 datanode" >>/home/ubuntu/datanode/datanode_hosts.conf
 cd /home/ubuntu/datanode
 sudo docker pull jafarbadour/datanode-dfs:latest
 sudo docker run -v $(pwd):/datanode -it -d --network host jafarbadour/datanode-dfs:latest
