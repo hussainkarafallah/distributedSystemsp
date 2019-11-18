aa4436d8e8ecd518395ee2afb6d810b560df9b4f
jafar <jafer.ba@Gmail.com>
2019-11-18 21:48:10 +0300

    ADDED CONFIGURATION ON AWS

--- BashesForDFSnodes/namenode.sh
+++ BashesForDFSnodes/namenode.sh
@@ -4,5 +4,7 @@ mkdir /home/ubuntu/namenode
 echo "26200" >>/home/ubuntu/namenode/namenode_hosts.conf
 echo "{\"hussain\":\"123456\",\"almir\":\"123456\",\"jafar\":\"123456\"}" >>/home/ubuntu/namenode/users.conf
 cd /home/ubuntu
-sudo docker run -v $(pwd):/namenode -it jafarbadour/namenode-dfs
+sudo docker run -v $(pwd):/namenode -d -it jafarbadour/namenode-dfs
+echo "GREAT SUCCESS NAMENODE IS RUNNING"
+
 
