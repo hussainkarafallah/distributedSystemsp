e54ddd2d55eecbbb3e69886f1eb57c6816cf7d44
jafar <jafer.ba@Gmail.com>
2019-11-18 21:07:28 +0300

    ADDED CONFIGURATION ON AWS

---
+++ BashesForDFSnodes/client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+snap install docker
+mkdir /home/ubuntu/client
+echo "18.217.199.246 26200 client" >>/home/ubuntu/client/client_hosts.conf
---
+++ BashesForDFSnodes/namenode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+snap install docker
+mkdir /home/ubuntu/namenode
+echo "26200" >>/home/ubuntu/namenode/namenode_hosts.conf
+echo "{\"hussain\":\"123456\",\"almir\":\"123456\",\"jafar\":\"123456\"}" >>/home/ubuntu/namenode/users.conf
+
